4a014f180c64f5cac0034a75a0698e1cf44e2cf6
A.hassan <102193302+Dbrown243@users.noreply.github.com>
2022-10-25 16:58:07 +0200

    a, b, c, d, e er gjort

--- oppgavefil.py
+++ oppgavefil.py
@@ -1,1 +1,20 @@
+from calendar import leapdays
+from datetime import datetime
+from turtle import title
+
+
 class Avtale:
+    def __init__(self, tittel, sted, tidspunkt, varighet):
+        self.tittel = tittel
+        self.sted = sted
+        self.tidspunkt = tidspunkt
+        self.varighet = varighet
+
+    def __str__(self):
+        return f"{self.tittel}, {self.sted}, {self.tidspunkt}, {self.varighet}"
+
+
+   ' def ny_avtale(ny_avtale):'
+
+
+
