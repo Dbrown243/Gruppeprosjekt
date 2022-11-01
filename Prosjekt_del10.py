d6d6fdbded74bff5a4ef384290f8809c02635829
Albi-Bashy <Bashar_041@outlook.com>
2022-11-01 17:34:35 +0100

    finished A

--- Prosjekt_del10.py
+++ Prosjekt_del10.py
@@ -4,11 +4,25 @@
 #Bigman best    : Abdiazizzzz Hassan
 
 # a)
+from mimetypes import init
 
+class Kategori:
+    def __init__(self, id, navn, prioritet=1):
+        self.id
+        self.navn
+        self.prioritet
 
+    def __str__(self):
+        resultat= Kategori + self.id + self.navn + self.prioritet
+        return resultat
 
 # b)
 
+
+  def ny_funksjon
+    Lag en funksjon som leser inn en ny kategori til systemet fra brukeren. Funksjonen skal bruke 
+input-setninger til å spørre brukeren om id, navn or prioritet for kategorien. Funksjonen skal 
+returnere et objekt av typen kategori.
 
 
 # c)
